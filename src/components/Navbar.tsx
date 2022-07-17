6cca873fd0601ec71430271f3a22a897b8aacc4f
Sabuhi0 <sabuhiq0@gmail.com>
2022-07-17 17:55:57 +0400

    Add tailwind css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,16 @@ import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <ul>
+    <div className='shadow-md'>
+      <div className='container mx-auto'>
+      <nav className='p-4'>
+        <ul className='flex gap-2 text-2xl'>
           <NavLink to='/'>Home</NavLink>
           <NavLink to='/store'>Store</NavLink>
           <NavLink to='/about'>About</NavLink>
         </ul>
       </nav>
+    </div>
     </div>
   )
 }
