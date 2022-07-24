7316f2526fe6a3ed3f8ab0c5c0a390a98ec6eee1
Sabuhi0 <sabuhiq0@gmail.com>
2022-07-25 00:26:18 +0400

    Add img and body section

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -7,10 +7,14 @@ type StoreItemProps = {
   image: string
 }
 
-const StoreItem = () => {
+const StoreItem = ({id, name, price, image}: StoreItemProps) => {
   return (
     <div className=''>
-      
+      <img className='h-full object-cover' src={image} alt="" />
+      <div className='bg-white flex justify-between items-center p-5'>
+        <span className='text-2xl'>{name}</span>
+        <span className='text-xl text-gray-600'>${price}</span>
+      </div>
     </div>
   )
 }
