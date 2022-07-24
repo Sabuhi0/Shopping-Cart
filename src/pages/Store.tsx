aa112a4b7ee7883d2ed03c39a5d9794c190ab9b4
Sabuhi0 <sabuhiq0@gmail.com>
2022-07-24 14:18:04 +0400

    Update store component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import StoreItem from '../components/StoreItem';
+import storeItems from '../data/data.json';
 
 const Store = () => {
   return (
-    <div>
-      Store
+    <div className='bg-[#f8f9fa] h-screen'>
+      <div className='container mx-auto'>
+        <h1 className='text-3xl py-3'>Store</h1>
+        <div className="grid gap-5 first-line:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {storeItems.map(item => (
+            <StoreItem key={item.id} {...item} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
