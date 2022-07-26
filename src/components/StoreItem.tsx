5b72162e1822a5dee827da73e1262386abd67b82
Sabuhi0 <sabuhiq0@gmail.com>
2022-07-26 13:14:16 +0400

    Update cart degin

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { formatCurrency } from '../utilities/formatCurrency'
 
 type StoreItemProps = {
   id: number,
@@ -8,14 +9,33 @@ type StoreItemProps = {
 }
 
 const StoreItem = ({id, name, price, image}: StoreItemProps) => {
+  const quantity = 1;
   return (
-    <div className=''>
-      <img className='h-full object-cover' src={image} alt="" />
-      <div className='bg-white flex justify-between items-center p-5'>
-        <span className='text-2xl'>{name}</span>
-        <span className='text-xl text-gray-600'>${price}</span>
+    <section>
+      <img className='w-full' src={image} alt="" />
+      <div className='bg-white p-5 rounded-br-xl rounded-bl-xl'>
+        <div className='flex justify-between items-center'>
+          <span className='text-2xl'>{name}</span>
+          <span className='text-xl text-gray-600'>{formatCurrency(price)}</span>
+        </div>
+        <div className='mt-4'>
+          {quantity === 0 ? (
+            <button className='w-full p-2 text-center rounded-sm bg-slate-500 text-white'>Add To Cart</button>
+          ): <div className='flex flex-col items-center gap-2'>
+              <div className='flex gap-4'>
+                <button>-</button>
+                <h1>
+                  <span className='text-2xl'>{quantity}</span> in cart
+                </h1>
+                <button>+</button>
+              </div>
+              <div className='flex items-center justify-center gap-2'>
+                <button>Remove</button>
+              </div>
+            </div>}
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
