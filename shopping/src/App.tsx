960b4611b47d3c75ade2ec685e3568d1081bfba5
Sabuhi0 <sabuhiq0@gmail.com>
2022-07-28 19:57:12 +0400

    Import Shopping Cart Provider

--- shopping/src/App.tsx
+++ shopping/src/App.tsx
@@ -3,17 +3,18 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import Navbar from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 const App = () => {
   return (
-    <>
+    <ShoppingCartProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/store" element={<Store />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </>
+    </ShoppingCartProvider>
   )
 }
 
