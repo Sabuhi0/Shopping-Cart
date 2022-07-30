b2776797751463c70c09b4f61108efc186dba492
Sabuhi0 <sabuhiq0@gmail.com>
2022-07-31 00:53:55 +0400

    Update Shopping cart component

--- shopping/src/components/ShoppingCart.tsx
+++ shopping/src/components/ShoppingCart.tsx
@@ -1,8 +1,27 @@
+import { Offcanvas } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
-const ShoppingCart = () => {
-  return (
-    <div>ShoppingCart</div>
-  )
+type ShoppingCartProps = {
+    isOpen: boolean
+}
+
+const ShoppingCart = ({isOpen}: ShoppingCartProps) => {
+    const { closeCart } = useShoppingCart()
+    return (
+        // <Offcanvas show={true} onHide={closeCart} placement="end">
+        //     <Offcanvas.Header closeButton>
+        //         <Offcanvas.Title>
+        //             Cart
+        //         </Offcanvas.Title>
+        //     </Offcanvas.Header>
+        // </Offcanvas>
+        isOpen ? <div className="bg-white p-6 w-[90%] h-full absolute top-0 right-0 transition duration-700 ease-in-out">
+            <div className="flex items-center justify-between">
+                <h1 className="text-2xl">Cart</h1>
+                <button className="bg-black text-white rounded-full w-6 h-6 flex items-center justify-center cursor-pointer" onClick={closeCart}>X</button>
+            </div>
+        </div>: null
+    )
 }
 
 export default ShoppingCart;
