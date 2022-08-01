04c9da4155949488f08a20e0cbf2fc8ac35aca5b
Sabuhi0 <sabuhiq0@gmail.com>
2022-08-02 00:43:18 +0400

    Remove Offcanvas bootstrap component

--- shopping/src/components/ShoppingCart.tsx
+++ shopping/src/components/ShoppingCart.tsx
@@ -1,4 +1,3 @@
-import { Offcanvas } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { FaRegWindowClose } from 'react-icons/fa'
 
@@ -9,13 +8,6 @@ type ShoppingCartProps = {
 const ShoppingCart = ({isOpen}: ShoppingCartProps) => {
     const { closeCart } = useShoppingCart()
     return (
-        // <Offcanvas show={true} onHide={closeCart} placement="end">
-        //     <Offcanvas.Header closeButton>
-        //         <Offcanvas.Title>
-        //             Cart
-        //         </Offcanvas.Title>
-        //     </Offcanvas.Header>
-        // </Offcanvas>
         isOpen ? <div className="bg-white p-6 w-[90%] h-full absolute top-0 right-0 transition duration-700 ease-in-out">
             <div className="flex items-center justify-between">
                 <h1 className="text-2xl">Cart</h1>
