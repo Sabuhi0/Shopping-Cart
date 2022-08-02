cc78e020c5d76a09f343c7546df7e44996d9e895
Sabuhi0 <sabuhiq0@gmail.com>
2022-08-03 00:04:59 +0400

    Add cart item

--- shopping/src/components/ShoppingCart.tsx
+++ shopping/src/components/ShoppingCart.tsx
@@ -1,18 +1,24 @@
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { FaRegWindowClose } from 'react-icons/fa'
+import CartItem from "./CartItem";
 
 type ShoppingCartProps = {
     isOpen: boolean
 }
 
 const ShoppingCart = ({isOpen}: ShoppingCartProps) => {
-    const { closeCart } = useShoppingCart()
+    const { closeCart, cartItems } = useShoppingCart()
     return (
         isOpen ? <div className="bg-white p-6 w-[90%] h-full absolute top-0 right-0 transition duration-700 ease-in-out">
             <div className="flex items-center justify-between">
                 <h1 className="text-2xl">Cart</h1>
                 <button className="cursor-pointer hover:scale-110" onClick={closeCart}><FaRegWindowClose size={25}/></button>
-         </div>
+            </div>
+            <div className="pt-6">
+                {cartItems.map(item => (
+                    <CartItem key={item.id} {...item}/>
+                ))}
+            </div>
         </div>: null
     )
 }
