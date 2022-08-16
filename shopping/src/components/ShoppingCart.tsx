8bedcb872421c0e51805ac5a250afdf26461088c
Sabuhi0 <sabuhiq0@gmail.com>
2022-08-16 15:27:29 +0400

    Update shopping cart component

--- shopping/src/components/ShoppingCart.tsx
+++ shopping/src/components/ShoppingCart.tsx
@@ -11,7 +11,7 @@ type ShoppingCartProps = {
 const ShoppingCart = ({isOpen}: ShoppingCartProps) => {
     const { closeCart, cartItems } = useShoppingCart()
     return (
-        isOpen ? <div className="bg-white p-6 w-[90%] h-full absolute top-0 right-0 transition duration-700 ease-in-out">
+        isOpen ? <div className="bg-white p-6 w-[90%] absolute top-0 right-0 transition duration-700 ease-in-out !h-full">
             <div className="flex items-center justify-between">
                 <h1 className="text-2xl">Cart</h1>
                 <button className="cursor-pointer hover:scale-110" onClick={closeCart}><FaRegWindowClose size={25}/></button>
