45eab15161ecb6be7316329841e3f82598d02749
Sabuhi0 <sabuhiq0@gmail.com>
2022-08-17 16:49:42 +0400

    Update Store page

--- shopping/src/pages/Store.tsx
+++ shopping/src/pages/Store.tsx
@@ -3,7 +3,7 @@ import storeItems from '../data/data.json';
 
 const Store = () => {
   return (
-    <div className='bg-[#f8f9fa] h-screen '>
+    <div className='bg-[#f8f9fa] h-full'>
       <div className='container mx-auto'>
         <h1 className='text-3xl py-3'>Store</h1>
         <div className="grid gap-5 first-line:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
