b008f166c99bde837386cc75fbc62415695053eb
Sabuhi0 <sabuhiq0@gmail.com>
2022-08-19 16:54:11 +0400

    Update dtore page

--- shopping/src/pages/Store.tsx
+++ shopping/src/pages/Store.tsx
@@ -6,7 +6,7 @@ const Store = () => {
     <div className='bg-[#f8f9fa] h-full'>
       <div className='container mx-auto'>
         <h1 className='text-3xl py-3'>Store</h1>
-        <div className="grid gap-5 first-line:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-5 first-line:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pb-5">
           {storeItems.map(item => (
             <StoreItem key={item.id} {...item} />
           ))}
