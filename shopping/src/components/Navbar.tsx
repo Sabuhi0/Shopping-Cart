ff0b6e35c42e45a5be9aa6a44575bf12d5340d94
Sabuhi0 <sabuhiq0@gmail.com>
2022-08-22 23:06:34 +0400

    Update navbar

--- shopping/src/components/Navbar.tsx
+++ shopping/src/components/Navbar.tsx
@@ -8,13 +8,12 @@ const Navbar = () => {
   return (
     <div className='shadow-md'>
       <div className='container mx-auto flex justify-between items-center'>
-        <nav className='p-4'>
-          <ul className='flex gap-2 text-2xl'>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/store'>Store</NavLink>
-            <NavLink to='/about'>About</NavLink>
-          </ul>
-        </nav>
+        <div className='py-4 relative'>
+          <h1 className='flex gap-2 text-2xl'>
+            Shopping Cart
+          </h1>
+          <img className='absolute top-0 left-6 right-0 -z-10 opacity-20' src="https://www.nicepng.com/png/detail/231-2317666_cart-icon-moving-shopping-cart-icon.png" alt="" />
+        </div>
         {cartQuantity > 0 && (
           <button className='rounded-full w-12 h-12 relative' onClick={openCart}>
             <h1 className='w-6 h-6 rounded-full bg-red-600 text-white flex justify-center items-center absolute bottom-0 right-1 text-[15px]'>
