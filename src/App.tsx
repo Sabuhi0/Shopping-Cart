02459cce70f14287c46bd040b04a71f12b402941
Sabuhi0 <sabuhiq0@gmail.com>
2022-08-24 14:11:03 +0400

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { Routes, Route } from "react-router-dom";
-// import About from "./pages/About";
-// import Home from "./pages/Home";
 import Store from "./pages/Store";
 import Navbar from "./components/Navbar";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
@@ -10,9 +8,7 @@ const App = () => {
     <ShoppingCartProvider>
       <Navbar />
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<Store />} />
-        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </ShoppingCartProvider>
   )
